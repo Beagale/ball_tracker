89df814270eabd10772bf9def86bc1e0010745e3
Beagale <beagale01@gmail.com>
2025-10-28 22:59:18 +0000

    added Sav's dead reckoning code for testing

--- ball_tracker/follow_ball.py
+++ ball_tracker/follow_ball.py
@@ -1,22 +1,4 @@
-# UNCOMMENT EITHER (1) or (2) for testing
-
-# (1) TO TEST: Josh's follow_ball.py code
-# CAN RUN THIS FIRST TO TEST THE REGULAR BALL TRACKING W/O NAVIGATIONAL ALGORITHM
-# REVISED (NOT THE SAME CODE, CHANGED IF STATEMENT AND SPEED
-# Copyright 2023 Josh Newans
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# FROM SAV
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Point
@@ -34,14 +16,13 @@ class FollowBall(Node):
             10)
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
 
-
         self.declare_parameter("rcv_timeout_secs", 1.0)
-        self.declare_parameter("angular_chase_multiplier", 0.0) # 0.7
-        self.declare_parameter("forward_chase_speed", 0.0) # 0.1
-        self.declare_parameter("search_angular_speed", 0.0) # 0.5
+        self.declare_parameter("angular_chase_multiplier", 0.7)  # Changed from 0.0 to 0.7
+        self.declare_parameter("forward_chase_speed", 0.2)  # Changed from 0.0 to 0.2
+        self.declare_parameter("search_angular_speed", 0.5)  # Changed from 0.0 to 0.5
         self.declare_parameter("max_size_thresh", 0.1)
         self.declare_parameter("filter_value", 0.9)
-
+        self.declare_parameter("center_deadzone", 0.05)  # NEW: deadzone for "centered" detection
 
         self.rcv_timeout_secs = self.get_parameter('rcv_timeout_secs').get_parameter_value().double_value
         self.angular_chase_multiplier = self.get_parameter('angular_chase_multiplier').get_parameter_value().double_value
@@ -49,7 +30,7 @@ class FollowBall(Node):
         self.search_angular_speed = self.get_parameter('search_angular_speed').get_parameter_value().double_value
         self.max_size_thresh = self.get_parameter('max_size_thresh').get_parameter_value().double_value
         self.filter_value = self.get_parameter('filter_value').get_parameter_value().double_value
-
+        self.center_deadzone = self.get_parameter('center_deadzone').get_parameter_value().double_value
 
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -60,16 +41,24 @@ class FollowBall(Node):
     def timer_callback(self):
         msg = Twist()
         if (time.time() - self.lastrcvtime < self.rcv_timeout_secs):
-            self.get_logger().info('TARGET FOUND!!!') # TEST
-            self.get_logger().info('Target: {}'.format(self.target_val))
-            print(self.target_dist)
-            # if (self.target_dist < self.max_size_thresh): # TEST: Remove the if statement to keep bot moving forward
+            self.get_logger().info('TARGET FOUND!!!')
+            self.get_logger().info('Target X: {:.3f}, Dist: {:.3f}'.format(self.target_val, self.target_dist))
+            
+            # Apply deadzone - if ball is centered enough, don't rotate
+            if abs(self.target_val) < self.center_deadzone:
+                msg.angular.z = 0.0  # Go perfectly straight
+                self.get_logger().info('Ball centered - going straight!')
+            else:
+                msg.angular.z = -self.angular_chase_multiplier * self.target_val
+                self.get_logger().info('Adjusting angle: {:.3f}'.format(msg.angular.z))
+            
             msg.linear.x = self.forward_chase_speed
-            msg.angular.z = -self.angular_chase_multiplier*self.target_val # TEST: to see if bot will still rotate
+            
         else:
-            self.get_logger().info('TARGET LOST!!!!!') # TEST
-            self.get_logger().info('Target lost')
-            msg.angular.z = self.search_angular_speed # TEST: to see if bot will still rotate
+            self.get_logger().info('TARGET LOST!!!!!')
+            msg.angular.z = self.search_angular_speed
+            msg.linear.x = 0.0  # Stop moving forward when searching
+            
         self.publisher_.publish(msg)
 
     def listener_callback(self, msg):
@@ -77,7 +66,6 @@ class FollowBall(Node):
         self.target_val = self.target_val * f + msg.x * (1-f)
         self.target_dist = self.target_dist * f + msg.z * (1-f)
         self.lastrcvtime = time.time()
-        # self.get_logger().info('Received: {} {}'.format(msg.x, msg.y))
 
 
 def main(args=None):
@@ -86,6 +74,102 @@ def main(args=None):
     rclpy.spin(follow_ball)
     follow_ball.destroy_node()
     rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
+
+# UNCOMMENT EITHER (1) or (2) for testing
+
+# (1) TO TEST: (MODIFIED FOR TESTING) Josh's follow_ball.py code
+# CAN RUN THIS FIRST TO TEST THE REGULAR BALL TRACKING W/O NAVIGATIONAL ALGORITHM
+# REVISED (NOT THE SAME CODE, CHANGED IF STATEMENT AND SPEED
+# Copyright 2023 Josh Newans
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# import rclpy
+# from rclpy.node import Node
+# from geometry_msgs.msg import Point
+# from geometry_msgs.msg import Twist
+# import time
+
+# class FollowBall(Node):
+
+#     def __init__(self):
+#         super().__init__('follow_ball')
+#         self.subscription = self.create_subscription(
+#             Point,
+#             '/detected_ball',
+#             self.listener_callback,
+#             10)
+#         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+
+
+#         self.declare_parameter("rcv_timeout_secs", 1.0)
+#         self.declare_parameter("angular_chase_multiplier", 0.0) # 0.7
+#         self.declare_parameter("forward_chase_speed", 0.0) # 0.1
+#         self.declare_parameter("search_angular_speed", 0.0) # 0.5
+#         self.declare_parameter("max_size_thresh", 0.1)
+#         self.declare_parameter("filter_value", 0.9)
+
+
+#         self.rcv_timeout_secs = self.get_parameter('rcv_timeout_secs').get_parameter_value().double_value
+#         self.angular_chase_multiplier = self.get_parameter('angular_chase_multiplier').get_parameter_value().double_value
+#         self.forward_chase_speed = self.get_parameter('forward_chase_speed').get_parameter_value().double_value
+#         self.search_angular_speed = self.get_parameter('search_angular_speed').get_parameter_value().double_value
+#         self.max_size_thresh = self.get_parameter('max_size_thresh').get_parameter_value().double_value
+#         self.filter_value = self.get_parameter('filter_value').get_parameter_value().double_value
+
+
+#         timer_period = 0.1  # seconds
+#         self.timer = self.create_timer(timer_period, self.timer_callback)
+#         self.target_val = 0.0
+#         self.target_dist = 0.0
+#         self.lastrcvtime = time.time() - 10000
+
+#     def timer_callback(self):
+#         msg = Twist()
+#         if (time.time() - self.lastrcvtime < self.rcv_timeout_secs):
+#             self.get_logger().info('TARGET FOUND!!!') # TEST
+#             self.get_logger().info('Target: {}'.format(self.target_val))
+#             print(self.target_dist)
+#             # if (self.target_dist < self.max_size_thresh): # TEST: Remove the if statement to keep bot moving forward
+#             msg.linear.x = self.forward_chase_speed
+#             # msg.angular.z = -self.angular_chase_multiplier*self.target_val # TEST: to see if bot will still rotate
+#             self.get_logger().info(f'ANgular speed: {msg.angular.z:.3f} rad/s') # TEST: to get the angular speed
+#         else:
+#             self.get_logger().info('TARGET LOST!!!!!') # TEST
+#             self.get_logger().info('Target lost')
+#             self.get_logger().info(f'ANgular speed: {msg.angular.z:.3f} rad/s') # TEST: to get the angular speed
+#             # msg.angular.z = self.search_angular_speed # TEST: to see if bot will still rotate
+#         self.publisher_.publish(msg)
+
+#     def listener_callback(self, msg):
+#         f = self.filter_value
+#         self.target_val = self.target_val * f + msg.x * (1-f)
+#         # self.target_dist = self.target_dist * f + msg.z * (1-f) # TEST: to remove the angular correcting
+#         self.lastrcvtime = time.time()
+#         # self.get_logger().info('Received: {} {}'.format(msg.x, msg.y))
+
+
+# def main(args=None):
+#     rclpy.init(args=args)
+#     follow_ball = FollowBall()
+#     rclpy.spin(follow_ball)
+#     follow_ball.destroy_node()
+#     rclpy.shutdown()
 
 
 
